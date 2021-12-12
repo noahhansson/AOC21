8c207b6ae355dbbb678eb0c770ec822ef0cce9fa
noahhansson <hansson.noah@gmail.com>
2021-12-12 12:36:14 +0100

    day 12

--- 12.py
+++ 12.py
@@ -1,15 +1,6 @@
 import utils
 
 inpt = utils.read_input("12")
-""" inpt = [
-    "start-A",
-    "start-b",
-    "A-c",
-    "A-b",
-    "b-d",
-    "A-end",
-    "b-end",
-] """
 start_end = {"start", "end"}
 small_caves = set()
 large_caves = set()
@@ -21,18 +12,7 @@ for cave in (small_caves | large_caves | start_end):
     paths[cave] = connections
 
 
-def build_path(at, been, all_paths):
-    been = been.copy()
-    been.append(at)
-    if at == "end":
-        all_paths.append(been)
-    else:
-        possible_steps = paths[at]
-        possible_steps = [step for step in possible_steps if ((step in small_caves and step not in been) or (step not in small_caves)) and step != "start"]
-        if len(possible_steps):
-            [build_path(step, been, all_paths) for step in possible_steps]
-
-def build_path2(at, been, n_visits, all_paths):
+def build_path1(at = "start", been = [], n_visits =  {cave : 0 for cave in small_caves | large_caves | start_end}, all_paths = []):
     been = been.copy()
     n_visits = n_visits.copy()
     n_visits[at] += 1
@@ -41,24 +21,33 @@ def build_path2(at, been, n_visits, all_paths):
         all_paths.append(been)
     else:
         possible_steps = paths[at]
-        max_visits = 1 if any(n_visits[cave] > 1 for cave in small_caves) else 2
+        max_visits = 1
+        possible_steps = [step for step in possible_steps if ((step in small_caves and n_visits[step] < max_visits) or (step not in small_caves)) and step != "start"]
+        if len(possible_steps):
+            [build_path1(step, been, n_visits, all_paths) for step in possible_steps]
+
+def build_path2(at = "start", been = [], n_visits =  {cave : 0 for cave in small_caves | large_caves | start_end}, all_paths = []):
+    been = been.copy()
+    n_visits = n_visits.copy()
+    n_visits[at] += 1
+    been.append(at)
+    if at == "end":
+        all_paths.append(been)
+    else:
+        possible_steps = paths[at]
+        max_visits = 2 if all(n_visits[cave] < 2 for cave in small_caves) else 1
         possible_steps = [step for step in possible_steps if ((step in small_caves and n_visits[step] < max_visits) or (step not in small_caves)) and step != "start"]
         if len(possible_steps):
             [build_path2(step, been, n_visits, all_paths) for step in possible_steps]
 
 def get_first_solution():
-    at = "start"
-    been = []
     all_paths = []
-    build_path(at, been, all_paths)
+    build_path1(all_paths = all_paths)
     print(len(all_paths))
 
 def get_second_solution():
-    at = "start"
-    been = []
     all_paths = []
-    n_visits = {cave : 0 for cave in small_caves | large_caves | start_end}
-    build_path2(at, been, n_visits, all_paths)
+    build_path2(all_paths = all_paths)
     print(len(all_paths))
 
 if __name__ == "__main__":
